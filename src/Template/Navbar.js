11719086d0e72bb0c19e8db19cadcbac4b7e7595
Muhammad-Khairul-Kholqi <khairulkhlqi31@gmail.com>
2024-03-03 20:03:49 +0700

    responsive navbar

--- src/Template/Navbar.js
+++ src/Template/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "..//Style/StyleNavbar.css";
 
 import LogoWhite from "../Assets/logo-white.png";
 import { FaRegUser } from "react-icons/fa";
@@ -11,9 +12,9 @@ const Navbar = () => {
     return(
         <div>
             <div className="navbar flex justify-between py-[20px] px-[5%] bg-black text-white border-b-[1px] border-white items-center">
-                <div className="flex flex-wrap gap-[30px] items-center">
+                <div className="navbar-log-admin flex flex-wrap gap-[30px] items-center">
                     <div>
-                        <img src={LogoWhite} className="w-[30px]" />
+                        <img src={LogoWhite} className="logo w-[30px]" />
                     </div>
                     <div className= 'navbar-nav-link flex flex-wrap gap-[15px] text-[15px]' >
                         <Link>
@@ -48,23 +49,19 @@ const Navbar = () => {
                     </div>
                 </div>
 
-                <div className="flex gap-[15px] items-center">
+                <div className="admin-log flex gap-[15px] items-center">
                     <Link>
                         <div className = "border-solid border-[1px] border-white p-[6px] rounded-[50%] hover:bg-white hover:text-black" >
-                            <FaRegUser className="text-[15px]" />
+                            <FaRegUser className="admin-log text-[15px]" />
                         </div>
                     </Link>
 
                     <Link>
                         <div className = "border-solid border-[1px] border-white p-[6px] rounded-[50%] hover:bg-white hover:text-black" >
-                            <BiLogOut className="text-[15px]" />
+                            <BiLogOut className="admin-log text-[15px]" />
                         </div>
                     </Link>
                 </div>
-                {/* <div className="hidden text-[25px]">
-                    <HiMenuAlt3 id="hamburger-menu" style={{ display: showNav ? 'none' : 'block' }} onClick={toggleNav} />
-                    <IoClose id="close" style={{ display: showNav ? 'block' : 'none' }} onClick={toggleNav} />
-                </div> */}
             </div>
         </div>
     )
